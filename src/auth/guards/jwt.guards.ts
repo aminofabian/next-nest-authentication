7be0152d0e9acb1c64250e1927bdd23af071a141
aminofabian <aminofab@gmail.com>
2024-01-02 21:27:49 +0300

    added a jwt guard

---
+++ src/auth/guards/jwt.guards.ts
@@ -0,0 +1,34 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
+
+
+@Injectable()
+export class JwtGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {};
+  async canActivate(
+    context: ExecutionContext,
+  ): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) throw new UnauthorizedException();
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JwtSecretKey,
+      });
+      request['user'] = payload;
+    }
+    catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+  private extractTokenFromHeader(request: Request) {
+    const [type, token] = request.headers.authorization.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { JwtGuard } from 'src/auth/guards/jwt.guards';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
+  @UseGuards(JwtGuard)
   @Get(':id')
   async getUserProfile(@Param('id') id: number) {
     return await this.userService.findById(id);
